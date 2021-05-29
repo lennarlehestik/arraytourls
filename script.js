b34dd1da53deda8aea20163ba052826678e0ddc9
lennarlehestik <43551132+lennarlehestik@users.noreply.github.com>
2021-05-30 00:08:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const get = () => {
   }
   eval(document.getElementById('arr').value).forEach((i) => print(i))
 }
+
+const getclean = () => {
+  const print = (item) => {
+      var cleanitem = item.split("/ref")[0].split("?ascsubtag")[0].split("?linkcode")[0].split("?tag")[0].split("?ascsubtag")[0].split("?SubscriptionId")[0].split("?irgwc")[0].split("?u1")[0].split("?selected")[0].split("?irclickid")[0].split("?skuId")[0]
+      var a = document.createElement('a');
+      var linkText = document.createTextNode(cleanitem);
+      a.appendChild(linkText);
+      a.title = item;
+    a.target = "_blank"
+      a.href = cleanitem;
+      document.body.appendChild(a);
+  }
+  eval(document.getElementById('arr').value).forEach((i) => print(i))
+}
